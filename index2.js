a28efe43b8664d98708736edd367cc495b62c783
jonathanflores444 <jonathan.flores289@ist17dejulio.edu.ec>
2024-05-02 11:14:24 -0500

    simulacion conejos

--- index2.js
+++ index2.js
@@ -61,7 +61,7 @@ app.get('/nombre',(req,res)=>{
     res.send('ALOHA!!!!'+" "+datoObtener);
 })
 //8vo web service (se captura de la url pero el parametro no esta definido (suma))
-//http://localhost:3000/suma4?n1=12?n2=12
+//http://localhost:3000/suma4?n1=12&n2=12
 app.get('/suma4',(req,res)=>{
     //me ayuda a obtener informacion no definida
     const num1 = parseInt(req.query.n1);
@@ -69,5 +69,62 @@ app.get('/suma4',(req,res)=>{
     var sumatot = num1 + num2;
     res.send(sumatot+"");
 })
+// 9no web service resta sin parametros y json
+app.get('/resta2',(req,res)=>{
+    const num1 = parseInt(req.query.n1);
+    const num2 = parseInt(req.query.n2);
+    var result=0;
+    if(n1>n2){
+        result=num1-num2;
+    }else{
+        result=num2-num1;
+    }
+    const resultado={total:result};
+    res.json(resultado);
+})
+
+//10 conejos
+//http:3000/conejos?p=3&np=5&nc=3&tm=20
+app.get('/conejos',(req,res)=>{
+    var pobActual=0;
+    var pobMuere=0;
+    var pobTotal=0;
+    var parejas=0;
+
+    //periodo
+    const periodo = parseInt(req.query.p)
+    //numero de parejas
+    const numParejas = parseInt(req.query.np)
+    //numero de crias
+    const numCrias = parseInt(req.query.nc)
+    //tasa de mortalidad
+    const tasMortal = parseInt(req.query.tm)
+
+    for(let i=0; i< periodo; i++){
+        if(i==0){
+            pobActual = numParejas * 2;
+            pobMuere = pobActual * tasMortal/100;
+            pobTotal = pobActual - pobMuere;
+        }else{
+            numCrias = numParejas*numCrias
+            pobActual += numCrias;
+            pobMuere = pobActual * tasMortal/100;
+            pobTotal = pobActual - pobMuere; 
+            parejas = pobTotal/2;
+        }
+        const resultados={
+            pobAnual:pobActual,
+            pobMorir:pobMuere,
+            pobRestante:pobTotal,
+            NParejas:parejas,
+            Ncrias:numCrias
+        };
+        res.json(resultados);
+        
+    }
+
+
+})
+
 //servicio que escuche por el puerto 3000
 app.listen(3000)
